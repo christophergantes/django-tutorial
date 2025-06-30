855efa4bc74aecc4c6d2c7a277ea08a14ba3c719
Christopher Gantes <78065304+christophergantes@users.noreply.github.com>
2025-06-30 07:26:21 -0400

    added post model and created post page

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(request):
-  # return HttpResponse("Hello World!");
 
-  return render(request, 'home.html')
+def homepage(request):
+    # return HttpResponse("Hello World!");
+
+    return render(request, "home.html")
+
 
 def about(request):
-  # return HttpResponse("Sup! Here's some information about me")
+    # return HttpResponse("Sup! Here's some information about me")
 
-  return render(request, 'about.html')
+    return render(request, "about.html")
